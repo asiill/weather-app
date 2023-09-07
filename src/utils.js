3cc5f6b8002adc890d4b5cf670f010cf28be8f21
asil <9745019+asiill@users.noreply.github.com>
2023-09-07 13:16:22 -0400

    Ensure hours and minutes are displayed in a 2 digit format

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,13 @@ const getLocalTime = (timeZone) => {
     let utc = time + (date.getTimezoneOffset() * 60000);
     let localT = utc + (timeZone * 1000);
     let localDT = new Date(localT);
-    let formattedTime = `${localDT.getHours()} h ${localDT.getMinutes()}`;
+
+    let hours = localDT.getHours();
+    hours = (hours < 10 ? "0" : "") + hours;
+    let minutes = localDT.getMinutes();
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+
+    let formattedTime = `${hours} h ${minutes}`;
     return formattedTime;
 }
 
